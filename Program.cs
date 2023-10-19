84ae798391ccb63047619e231d669201c3fe2d59
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2023-10-19 14:27:29 +0200

    Made code more efficient and fixed code so you can generate larger numbers without turning negative

--- Program.cs
+++ Program.cs
@@ -4,8 +4,11 @@ namespace fnum;
 
 public static partial class Program
 {
+    [GeneratedRegex("^(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[012])\\d{7}$")]
+    private static partial Regex FnumRegex();
+
     // Check if a Fnum is valid    
-    private static bool FnumIsValid(IReadOnlyList<ushort> fnumList)
+    private static bool CheckFnumValid(IReadOnlyList<ushort> fnumList)
     {
         var fnum = "";
         ushort[] check1 = { 3, 7, 6, 1, 8, 9, 4, 5, 2, 1, 0 };
@@ -21,7 +24,7 @@ public static partial class Program
             checksum2 += (ushort)(fnumList[i] * check2[i]);
         }
 
-        var regexOk = MyRegex().IsMatch(fnum);
+        var regexOk = FnumRegex().IsMatch(fnum);
         return regexOk && CleanDivide(checksum1, 11) && CleanDivide(checksum2, 11);
     }
 
@@ -32,6 +35,46 @@ public static partial class Program
         // ReSharper disable once CompareOfFloatsByEqualityOperator
         return (float)n1 / n2 == n1 / n2; // Same as (return n1 % n2 == 0)
     }
+
+    // Increase the value of fnum
+    private static string IncreaseFnum(string fnum)
+    {
+        var num = ulong.Parse(fnum) + 1;
+        return num.ToString("00000000000");
+    }
+
+    // Generate all fnums
+    private static void GenerateFnums()
+    {
+        var fnum = "01010000000";
+
+        do
+        {
+            // Increase the fnum by 1
+            fnum = IncreaseFnum(fnum);
+
+            // Adding each number in fnum to fnumList
+            var fnumList = new List<ushort>();
+            foreach (var ch in fnum)
+            {
+                if (ushort.TryParse(ch.ToString(), out var result)) fnumList.Add(result);
+            }
+
+            // Check if month is 13 then change the fnum to be 01 again to not try for months above 13
+            var month = int.Parse(fnumList[2] + "" + fnumList[3]);
+            if (month == 13)
+            {
+                var date = int.Parse(fnumList[0] + "" + fnumList[1]) + 1;
+                fnum = date + "010000000";
+            }
+
+            // Skipping rest of code if fnum is invalid
+            if (!CheckFnumValid(fnumList)) continue;
+
+            Console.WriteLine(fnum);
+        } while (ulong.Parse(fnum) <= 31130000000);
+    }
+
 
     // Checking if a Fnum is valid and writing to Console
     private static void WriteFnumIsValid()
@@ -46,44 +89,12 @@ public static partial class Program
             }
         }
 
-        Console.WriteLine(FnumIsValid(fnumList));
-    }
-
-    // Generates a fnum
-    private static string GenerateFnum(string fnum)
-    {
-        var num = int.Parse(fnum) + 1;
-        return num.ToString("00000000000");
+        Console.WriteLine(CheckFnumValid(fnumList));
     }
 
     private static void Main()
     {
         // WriteFnumIsValid();
-        
-        
-        var fnum = "01120000000";
-        
-        // for (float i = 0; i < 99999999999; i++)
-        do
-        {
-            fnum = GenerateFnum(fnum);
-        
-            var fnumList = new List<ushort>();
-            foreach (var ch in fnum)
-            {
-                if (ushort.TryParse(ch.ToString(), out var result))
-                {
-                    fnumList.Add(result);
-                }
-            }
-        
-            if (!FnumIsValid(fnumList)) continue;
-        
-            Console.WriteLine(fnum);
-            Console.WriteLine(fnum[2] + "" + fnum[3]);
-        } while (long.Parse(fnum) <= 31130000000);
+        GenerateFnums();
     }
-
-    [GeneratedRegex("^(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[012])\\d{7}$")]
-    private static partial Regex MyRegex();
 }
